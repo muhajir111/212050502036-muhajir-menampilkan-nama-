736ef5a6bfd63a72eb80b878b7917c00622a67d8
muhajir111 <104090571+muhajir111@users.noreply.github.com>
2022-05-24 22:51:44 +0800

    Add files via upload

---
+++ App.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.muhajir.tugasiv;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class App {
+
+
+    public static void main(String[] args) {
+        int tampilkannama = 450;
+       
+        
+        if (tampilkannama < 100 )
+        {
+        System.out.println("fiki");
+        }
+         
+       else if (tampilkannama < 200)
+        {   
+         System.out.println("tegar");
+        }
+       else if (tampilkannama < 300)
+        {   
+         System.out.println("sartol");
+        }
+       else if (tampilkannama < 400) 
+        {
+         System.out.println("avatar");
+        }
+         
+         
+   
+    }
+}
